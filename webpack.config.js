8022cb1309ac37a5c5ccd128c768cf3a4813c276
mimo <viktor.bro.js@gmail.com>
2026-07-21 20:32:53 +0300

    Fix production build - use relative paths
    - Change publicPath to './' for production builds
    - Keeps '/' for development (webpack dev server)
    - Fixes white screen in AppImage/snap builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-    publicPath: '/'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    })
-  ],
-  devServer: {
-    static: false,
-    port: 3000,
-    hot: true,
-    open: false,
-    historyApiFallback: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './src/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      clean: true,
+      publicPath: isProduction ? './' : '/'
     },
-    onAfterSetupMiddleware: (devServer) => {
-      devServer.middleware.waitUntilValid(() => {
-        console.log('\n✅ Webpack dev server is ready!\n');
-      });
+    devtool: isProduction ? false : 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+      })
+    ],
+    devServer: {
+      static: false,
+      port: 3000,
+      hot: true,
+      open: false,
+      historyApiFallback: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
     }
-  }
+  };
 };
